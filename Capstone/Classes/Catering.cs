9f07a0e6ffdbfe128939d6a8c97adb877dbe9e7b
Chris Rivera <47066145+Chris-M-Rivera@users.noreply.github.com>
2022-03-21 18:36:36 -0400

    first commit

---
+++ Capstone/Classes/Catering.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class Catering
+    {
+
+        public Catering()
+        {
+            ItemDirectory = Data.GetItemsFromFile();
+        }
+
+        public List<CateringItem> ItemDirectory = new List<CateringItem>();
+
+        public List<CateringItem> ShoppingCart = new List<CateringItem>();
+
+        public FileAccess Data = new FileAccess();
+
+        private double Balance = 0;
+
+
+        public CateringItem[] GetItems()
+        {
+            CateringItem[] result = new CateringItem[ItemDirectory.Count];
+
+            ItemDirectory.Sort((x, y) => x.ProductCode.CompareTo(y.ProductCode));
+
+            for (int i = 0; i < ItemDirectory.Count; i++)
+            {
+                result[i] = ItemDirectory[i];
+            }
+            return result;
+        }
+
+        public bool AddMoney(double money)
+        {
+            if (Balance + money > 1500)
+            {
+                return false;
+            }
+            else
+            {
+                Balance += money;
+                Data.AuditAddMoney(money, Balance);
+                return true;
+            }
+        }
+
+        public double ReturnCurrentBalance()
+        {
+            return Balance;
+        }
+
+
+        public bool DoesProductExist(string productCode)
+        {
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == productCode)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public CateringItem ConvertCodeToItem(string productCode)
+        {
+            CateringItem result = new CateringItem();
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == productCode)
+                {
+                    result.Name = item.Name;
+                    result.Price = item.Price;
+                    result.Type = item.Type;
+                    result.ProductCode = item.ProductCode;
+                    result.Quantity = item.Quantity;
+                }
+            }
+            return result;
+        }
+
+        public bool SoldOutChecker(CateringItem wantedItem)
+        {
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == wantedItem.ProductCode)
+                {
+                    if (item.Quantity == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool SufficientStock(CateringItem wantedItem, int quantityOfProduct)
+        {
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == wantedItem.ProductCode)
+                {
+                    if (item.Quantity >= quantityOfProduct)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool SufficientFundsCheck(CateringItem wantedItem, int quantityOfProduct)
+        {
+            double wantedItemsValue = 0;
+
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == wantedItem.ProductCode)
+                {
+                    wantedItemsValue = (item.Price * quantityOfProduct);
+                }
+            }
+
+            if ((wantedItemsValue) < Balance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsItemInShoppingCart(CateringItem wantedItem)
+        {
+            bool result = false;
+            foreach (CateringItem item in ShoppingCart)
+            {
+                if (item.ProductCode == wantedItem.ProductCode)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public void MoveItemsToCart(CateringItem wantedItem, int quantityOfProduct)
+        {
+            if (IsItemInShoppingCart(wantedItem))
+            {
+                foreach (CateringItem item in ShoppingCart)
+                {
+                    if (item.ProductCode == wantedItem.ProductCode)
+                    {
+                        item.Quantity += quantityOfProduct;
+                    }
+                }
+            }
+            else
+            {
+                wantedItem.Quantity = quantityOfProduct;
+                ShoppingCart.Add(wantedItem);
+            }
+            foreach (CateringItem item in ItemDirectory)
+            {
+                if (item.ProductCode == wantedItem.ProductCode)
+                {
+                    item.Quantity -= quantityOfProduct;
+                }
+            }
+            Balance -= (wantedItem.Price * quantityOfProduct);
+            Data.AuditItemsBought(quantityOfProduct, wantedItem.Name, wantedItem.ProductCode, (wantedItem.Price * quantityOfProduct), Balance);
+        }
+
+        public string[] Receipt()
+        {
+            string[] result = new string[ShoppingCart.Count];
+
+            for (int i = 0; i < ShoppingCart.Count; i++)
+            {
+                string itemType = "";
+                string message = "";
+                switch (ShoppingCart[i].Type)
+                {
+                    case "A":
+                        itemType = "Appetizer";
+                        message = "You might need extra plates.";
+                        break;
+                    case "B":
+                        itemType = "Beverage";
+                        message = "Don't forget ice.";
+                        break;
+                    case "D":
+                        itemType = "Dessert";
+                        message = "Coffee goes with dessert.";
+                        break;
+                    case "E":
+                        itemType = "Entree";
+                        message = "Did you remember dessert.";
+                        break;
+                }
+
+                string quantity = ShoppingCart[i].Quantity.ToString();
+                string price = ShoppingCart[i].Price.ToString("C");
+                string totalCost = (ShoppingCart[i].Price * ShoppingCart[i].Quantity).ToString("C");
+
+                result[i] = ($"{quantity.PadRight(5, ' ')}{itemType.PadRight(15, ' ')}{ShoppingCart[i].Name.PadRight(25, ' ')}{price.PadRight(8, ' ')}{totalCost.PadLeft(8, ' ')}  {message}");
+            }
+            return result;
+        }
+
+        public bool ValidBillCheck(double depositedBill)
+        {
+            if((depositedBill == 1) || (depositedBill == 5) || (depositedBill == 10) || (depositedBill == 20) || (depositedBill == 50) || (depositedBill == 100))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double AmountDue()
+        {
+            double result = 0;
+            foreach (CateringItem item in ShoppingCart)
+            {
+                result += (item.Price * item.Quantity);
+            }
+            return result;
+        }
+
+        public string ChangeToReturn()
+        {
+            int sumOfCart = 0;
+            int runningTotal = sumOfCart + (int)(Balance * 100);
+            string result = "You received";
+            
+            foreach (CateringItem item in ShoppingCart)
+            {
+                sumOfCart += (int)(Math.Round((item.Price * item.Quantity), 2) * 100);
+            }
+
+            Dictionary<string, int> change = new Dictionary<string, int>();
+
+            change.Add("Hundreds",(runningTotal / 10000));
+            runningTotal %= 10000;
+            change.Add("Fifties", (runningTotal / 5000));
+            runningTotal %= 5000;
+            change.Add("Twenties", (runningTotal / 2000));
+            runningTotal %= 2000;
+            change.Add("Tens", (runningTotal / 1000));
+            runningTotal %= 1000;
+            change.Add("Fives", (runningTotal / 500));
+            runningTotal %= 500;
+            change.Add("Ones", (runningTotal / 100));
+            runningTotal %= 100;
+            change.Add("Quarters", (runningTotal / 25));
+            runningTotal %= 25;
+            change.Add("Dimes", (runningTotal / 10));
+            runningTotal %= 10;
+            change.Add("Nickels", (runningTotal / 5));
+
+            Data.AuditGiveChange(runningTotal);
+
+            foreach(KeyValuePair<string,int> kvp in change)
+            {
+                if (kvp.Value != 0)
+                {
+                    result += ($" ({kvp.Value}) {kvp.Key},");
+                }
+            }
+
+            result = result.Substring(0, result.Length - 1) + " in change.";
+
+            return result;
+        }
+
+        public void EndOfTransaction()
+        {
+            Balance = 0;
+            ShoppingCart.Clear();
+        }
+    }
+}
---
+++ Capstone/Classes/UserInterface.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Capstone.Classes
+{
+    public class UserInterface
+    {
+        private Catering catering = new Catering();
+
+        public void RunInterface()
+        {
+
+            bool done = false;
+            
+            while (!done)
+            {
+                MainMenuText();
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        DisplayUpdatedListOfItems();
+                        break;
+                    case "2":
+                        SubMenu();
+                        break;
+                    case "3":
+                        done = true;
+                        break;
+                    default:
+                        PrintRed("Please enter a valid choice.");
+                        break;
+                }
+            }
+        }
+        public void SubMenu()
+        {
+            bool done = false;
+
+            while (!done)
+            {
+                SubMenuText();
+
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        AddMoneyText();
+                        break;
+                    case "2":
+                        DisplayUpdatedListOfItems();
+                        AddToCartText();                        
+                        break;
+                    case "3":
+                        DisplayReciept();
+                        catering.EndOfTransaction();
+                        done = true;
+                        break;
+                    default:
+                        PrintRed("Please enter a valid choice.");
+                        break;
+                }
+            }
+        }
+
+
+        public void MainMenuText()
+        {
+            Console.WriteLine();
+            Console.WriteLine("(1) Display Catering Items");
+            Console.WriteLine("(2) Order");
+            Console.WriteLine("(3) Quit");
+            Console.WriteLine();
+        }
+
+        public void SubMenuText()
+        {
+            Console.WriteLine();
+            Console.WriteLine("(1) Add Money");
+            Console.WriteLine("(2) Select Products");
+            Console.WriteLine("(3) Complete Transaction");
+            Console.WriteLine();
+            Console.Write("Current Account Balance: ");
+            PrintGreen($"{Math.Round(catering.ReturnCurrentBalance(), 2):C}");
+            Console.WriteLine();
+        }
+
+        public void DisplayUpdatedListOfItems()
+        {
+            CateringItem[] productsAvailable;
+
+            string productCode = "Product Code";
+            string description = "Description";
+            string quantity = "Qty";
+            string price = "Price";
+
+            Console.WriteLine();
+            Console.WriteLine($"{productCode.PadRight(15, ' ')}{description.PadRight(25, ' ')}{quantity.PadRight(10, ' ')}{price.PadRight(15, ' ')}");
+
+            productsAvailable = catering.GetItems();
+
+            foreach (CateringItem item in productsAvailable)
+            {
+                string priceAsString = item.Price.ToString("C");
+
+                if (item.Quantity < 1)
+                {
+                    string soldOut = "SOLD OUT";
+                    Console.WriteLine($"{item.ProductCode.PadRight(15, ' ')}{item.Name.PadRight(25, ' ')}{soldOut.PadRight(10, ' ')}{priceAsString.PadRight(15, ' ')}");
+                }
+                else
+                {
+                    string quantityAsString = item.Quantity.ToString();
+                    Console.WriteLine($"{item.ProductCode.PadRight(15, ' ')}{item.Name.PadRight(25, ' ')}{quantityAsString.PadRight(10, ' ')}{priceAsString.PadRight(15, ' ')}");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void AddMoneyText()
+        {
+            Console.Write("Please insert bill (valid bill amounts are 1, 5, 10, 20, 50, 100): ");
+            double depositedBill = 0;
+            try
+            {
+                double tempDepositedBill = double.Parse(Console.ReadLine());
+                depositedBill = tempDepositedBill;
+            }
+            catch (FormatException ex)
+            {
+                PrintRed("Please enter a valid bill.");
+                Console.WriteLine();
+                return;
+            }
+
+            if (catering.ValidBillCheck(depositedBill))
+            {
+                bool successfulDeposit = catering.AddMoney(depositedBill);
+                if (!successfulDeposit)
+                {
+                    PrintRed("Balance may not go over $1500.");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                PrintRed("Please enter a valid bill.");
+                Console.WriteLine();
+            }
+        }
+
+        public void AddToCartText()
+        {
+            Console.Write("Please enter the product code: ");
+            string productCodeInput = Console.ReadLine().ToUpper();
+            CateringItem chosenCateringItem = catering.ConvertCodeToItem(productCodeInput);
+            bool exists = catering.DoesProductExist(productCodeInput);
+            if (!exists)
+            {
+                PrintRed("Product does not exist.");
+                return;
+            }
+
+            Console.Write("Please enter the quantity: ");
+            int quantityOfProducts = 0;
+
+            try
+            {
+                int tempQuantityOfProducts = int.Parse(Console.ReadLine());
+                quantityOfProducts = tempQuantityOfProducts;
+            }
+            catch (FormatException ex)
+            {
+                PrintRed("Invalid quantity.");
+                Console.WriteLine();
+                return;
+            }
+
+            bool soldOut = catering.SoldOutChecker(chosenCateringItem);
+            if (soldOut)
+            {
+                PrintRed("Item is SOLD OUT!");
+                Console.WriteLine();
+                return;
+            }
+            bool haveStock = catering.SufficientStock(chosenCateringItem, quantityOfProducts);
+            if (!haveStock)
+            {
+                PrintRed("Insufficient stock.");
+                Console.WriteLine();
+                return;
+            }
+            bool sufficentFunds = catering.SufficientFundsCheck(chosenCateringItem, quantityOfProducts);
+            if (!sufficentFunds)
+            {
+                PrintRed("Insufficient funds to add items.");
+                Console.WriteLine();
+                return;
+            }
+            catering.MoveItemsToCart(chosenCateringItem, quantityOfProducts);
+        }
+
+        public void DisplayReciept()
+        {
+            Console.WriteLine();
+            string[] recieptInfo = catering.Receipt();
+            for(int i = 0; i < recieptInfo.Length; i++)
+            {
+                Console.WriteLine(recieptInfo[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Total: {catering.AmountDue():C}");
+            Console.WriteLine();
+            Console.WriteLine(catering.ChangeToReturn());
+        }
+
+        internal void PrintGreen(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        internal void PrintRed(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
